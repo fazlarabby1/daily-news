9aadb35a34f3bd802d2a6edf30a0f364bc11bea3
fazlarabby1 <fazlarabby41@gmail.com>
2022-09-03 13:42:49 +0600

    icon navbar added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log('connected');
+const loadCategory = () =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.data.news_category))
+}
+loadCategory()
